05404490cbbf429aa1ae784447a46c033d44217b
olinemazumder <olinemozumder@gmail.com>
2021-11-03 10:38:02 +0600

    Added Home Page Section

--- client/src/Pages/Home/Home/Home.js
+++ client/src/Pages/Home/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navigation from "../../Shared/Navigation/Navigation";
 import Services from "../Services/Services";
+import AppointmentBanner from "../AppointmentBanner/AppointmentBanner";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
             <Navigation/>
+            <Banner/>
             <Services/>
+            <AppointmentBanner/>
         </div>
     );
 };
