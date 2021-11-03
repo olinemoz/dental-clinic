2ec4d5291ca7a6ddc3858cc28ec84100cbaff29e
olinemazumder <olinemozumder@gmail.com>
2021-11-03 08:38:04 +0600

    Project Setup

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
+import Home from "./Pages/Home/Home";
+
+function App() {
+    return (
+        <div className="App">
+          <Router>
+            <Switch>
+                 <Route exact path="/" component={Home}/>
+                 <Route exact path="/home" component={Home}>
+                   <Redirect to="/" />
+                 </Route>
+            </Switch>
+          </Router>
+        </div>
+    );
+}
+
+export default App;
